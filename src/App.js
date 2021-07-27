81f838384058a25358f6aa4327b5898b7043d2f5
MatheusGeanezi <matheus.geanezi@yahoo.com.br>
2021-07-27 14:39:52 -0300

    finalizado

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import Home from './Home.js'
-import { ThemeProvider, createMuiTheme, makeStyles } from '@material-ui/core'
+import React, { useState } from 'react';
+import { ThemeProvider, createMuiTheme, makeStyles } from '@material-ui/core';
+
+import Home from './Home';
+
+const useStyles = makeStyles({
+  root: {},
+});
 
 function App() {
+  const [darkMode, setDarkMode] = useState(false);
+
   const theme = createMuiTheme({
-    palette:{
-      primary:{
-        main:'#ffffff'
+    spacing: 4,
+    palette: {
+      type: darkMode ? 'dark' : 'light',
+      primary: {
+        main: '#f44336',
       },
-      secondary:{
-        main: '#3f51b5'
-      }
-    }
-  })
+      secondary: {
+        main: '#3EA6FF',
+      },
+      background: {
+        default: darkMode ? '#232323' : '#FFF',
+        dark: darkMode ? '#181818' : '#f4f6f8',
+        paper: darkMode ? '#232323' : '#FFF',
+      },
+    },
+  });
+
+  const classes = useStyles();
+
   return (
     <ThemeProvider theme={theme}>
-      <Home />
+      <Home darkMode={darkMode} setDarkMode={setDarkMode} />
     </ThemeProvider>
   );
 }
 
 export default App;
+
